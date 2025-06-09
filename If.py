48313d3d0dc341436b40784d56aed5f727ae580c
Muhammed Bin Al-Araby <mehmet@araby.tr>
2025-06-09 12:52:38 +0300

    ▲ Pyramids

---
+++ If.py
@@ -0,0 +1,31 @@
+import time;
+
+Value1 = 1002;
+Value2 = 1200;
+
+""""""
+
+if Value1 < Value2 :
+    print('Hello World');
+else :
+    print('Goodbye World');
+
+
+print('Hello World') if Value1 < Value2 else print('Goodbye World');
+
+""""""
+
+
+time.sleep(2);
+
+
+""""""
+
+i = 0;
+
+while i < 10 :
+    i += 1;
+
+print(i);
+
+""""""
